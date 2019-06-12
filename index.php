c75d9e58e0cb09cb8566b1aa1cf679a59009bdd9
dbhans <11444202+dbhans@users.noreply.github.com>
2019-06-11 23:42:21 -0400

    creation

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome</title>
+</head>
+<body>
+<h3>Information </h3>
+<p>
+This is a page for your final project here,<br> we can add information about the project 
+</p>
+<hr>
+    <ul>
+    <li><a href="page/display.php">List of installation</a></li>
+    <li><a href="page/form.php" >Enter new installation</a></li>
+    </ul>
+</body>
+</html>
---
+++ page/form.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+<h3>Software</h3>
+  <form action="" method="post">
+
+
+<div>
+<label for="name"> Name </label>
+<input type="text" name="name" id="name">
+</div>
+<div>
+<label for="description"> Description </label>
+<textarea name="description" id="" cols="30" rows="10"></textarea>
+</div>
+<br>
+<h3>Instalation</h3>
+<div>
+<label for="date"> date </label>
+<input type="date" name="date" id="date">
+</div>
+<div>
+<label for="enviroment"> Enviroment </label>
+<select name="enviroment" id="enviroment">
+    <option value="Windows"> Windows </option>
+    <option value="Unix"> Unix </option>
+    <option value="Linux"> Linux </option>
+    <option value="Windows Server">Windows Server</option>
+    <option value="Linux Server"> Linux Server</option>
+</select>
+</div>
+
+</form>
+</body>
+</html>
+  
